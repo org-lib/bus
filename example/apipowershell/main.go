1bea82800e297b7bb4e461f073bc17f40af888db
org-lib <shangguannihao@gmail.com>
2022-01-15 14:32:16 +0800

    chan修复

---
+++ example/apipowershell/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/org-lib/bus/config"
+	"github.com/org-lib/bus/cors"
+	"github.com/org-lib/bus/example/makeapp/handler"
+	"github.com/org-lib/bus/logger"
+	"github.com/org-lib/bus/pool"
+	"go.uber.org/zap"
+	"runtime"
+)
+
+var (
+	router     = gin.Default()
+	defaultMsg = `{"code": -1, "msg":"http: Handler timeout"}`
+	MaxProces  = runtime.NumCPU()
+)
+
+func main() {
+	e()
+	//并发能力控制
+
+	if MaxProces > 2 {
+		MaxProces -= 1
+	}
+	runtime.GOMAXPROCS(MaxProces)
+
+	// 设置gin启动模式为生产模式
+
+	gin.SetMode(gin.ReleaseMode)
+
+	//跨域
+	router.Use(cors.Cors())
+
+	router.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	//在线任务数
+	config.Work = pool.NewPool(config.Config.V.GetInt("pool.max"))
+
+	// 管理API
+	v1 := router.Group("api")
+	{
+		v1.GET("/hello", handler.HelloWorld)
+		v1.POST("/msi", handler.Gus)
+	}
+	config.Work.Wait()
+
+	// 启动服务，获取配置文件config.yaml的IP和端口：listen_ip和listen_port
+
+	addr := fmt.Sprintf("%v:%v", config.Config.V.GetString("server.listen_ip"), config.Config.V.GetString("server.listen_port"))
+
+	//输出一个名为message的自定义内容值、{"message":"Start server"}，以及自定义key：value 的输出
+	//{"level":"INFO","timestamp":"2021-12-22 13:38:09:000","caller":"example/main.go:68","message":"Start server","listen":"0.0.0.0:80"}
+
+	logger.Log.Info("Start server", zap.String("listen", addr))
+	err := router.Run(fmt.Sprintf("%v", addr))
+	if err != nil {
+		logger.Log.Error("Start server", zap.String("error", err.Error()))
+	}
+	//logger.Log.Info("Start server success", zap.String("listen", addr))
+
+}
+func e() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("*** 异常:", err)
+			logger.Log.Error("*** 异常:", zap.String("remote", fmt.Sprintf("%v", err)))
+		}
+	}()
+}
--- example/makeapp/handler/helloworld.go
+++ example/makeapp/handler/helloworld.go
@@ -24,40 +24,46 @@ type Message struct {
 
 func Gus(ctx *gin.Context) {
 	//全局异常抓捕
+
 	e()
 	//参数转 struct 对象
+
 	var msg Message
 
 	//定义一个chan,用作同步返回任务结果
+
 	ch := make(chan bool, 1)
 	//参数获取
+
 	if err := ctx.ShouldBindJSON(&msg); err != nil {
 		//	参数错误
+
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"msg": err.Error(),
 		})
 		//  程序退出
+
 		return
 	}
 
 	//开始任务
-	config.Work.Add(1)
-
 	//需要注意 chan 的使用方式
-	go makeApp(&msg, ch, func(result int, reason string) {
+
+	go makeApp(&msg, func(result int, reason string) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"msg":   reason,
 			"statu": result,
 		})
-		config.Work.Done()
+		config.Work.Add(1)
 		ch <- true
 	})
 	//结束任务
+
+	config.Work.Done()
 	<-ch
 }
 
-func makeApp(v interface{}, ch chan bool, res func(result int, reason string)) {
-
+func makeApp(v interface{}, res func(result int, reason string)) {
 	switch v.(type) {
 	//构建MSI
 	case *Message:
@@ -69,12 +75,7 @@ func makeApp(v interface{}, ch chan bool, res func(result int, reason string)) {
 			res(-1, "参数不正确，或者缺失必要参数！")
 			return
 		}
-		//do msi
-		//time.Sleep(time.Second * 10)
-		//for i := 0; i < len(objMsi.Commands); i++ {
-		//	fmt.Println(objMsi.Commands[i])
-		//}
-		err := doMsi(objMsi)
+		err, out := doMsi(objMsi)
 		if err != nil {
 			res(-1, fmt.Sprintf("Make MSI File Fail: %v", err))
 			return
@@ -87,7 +88,7 @@ func makeApp(v interface{}, ch chan bool, res func(result int, reason string)) {
 		//time.Sleep(time.Second * 1)
 
 		//返回任务处理状态
-		res(1, "MakeApp完成.")
+		res(1, fmt.Sprintf("MakeApp完成. 操作日志：%v", out))
 
 	default:
 		res(-1, "没有找到合适的任务与，请检查传入参数，或者查看README.md")
@@ -101,21 +102,23 @@ func e() {
 		}
 	}()
 }
-func doMsi(m msi.Msi) error {
+func doMsi(m msi.Msi) (error, string) {
+	var outStr string
 	shell, err := xshell.Powershell()
 	if err != nil {
-		return err
+		return err, ""
 	}
 	defer shell.Exit()
 
 	// ... 交互 in
 	for i := 0; i < len(m.Commands); i++ {
 		stdout, stderr, err := shell.Execute(m.Commands[i])
+		outStr = fmt.Sprintf("%v", stdout)
 		if err != nil {
 			logger.Log.Error("Making MSI File Error", zap.String("MSI stderr", stderr))
-			return err
+			return err, ""
 		}
 		logger.Log.Info("Making MSI File ", zap.String("MSI stdout", stdout))
 	}
-	return nil
+	return nil, outStr
 }
