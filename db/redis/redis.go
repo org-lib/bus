a02337bbd9586088876b06043174a6de69aac669
org-lib <96509210+org-lib@users.noreply.github.com>
2022-02-22 11:19:26 +0800

    redis info to map[string]interface

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"fmt"
 	"github.com/go-redis/redis"
+	"strings"
 )
 
 type Info struct {
@@ -27,4 +28,47 @@ func NewClient(cnf *Info) *redis.Client {
 		DB:       cnf.DB,
 	})
 	return client
+}
+
+func InfoToMap(client *redis.Client) (map[string]map[string]interface{}, error) {
+	msg, err := client.Info().Result()
+	if err != nil {
+		return nil, err
+	}
+	info := make(map[string]map[string]interface{})
+	subInfo := make(map[string]interface{})
+
+	// one end
+
+	info_title := ""
+	for _, s := range strings.Split(string(msg), "\r\n") {
+		if strings.Trim(s, " ") == "" {
+			continue
+		}
+		if strings.HasPrefix(s, "# ") {
+			if info_title != "" {
+				info[info_title] = subInfo
+
+				//重置子 map
+
+				subInfo = nil
+				subInfo = make(map[string]interface{})
+			}
+			info_title = strings.ReplaceAll(s, "# ", "")
+			continue
+		}
+		kv := strings.Split(s, ":")
+		subInfo[kv[0]] = kv[1]
+	}
+	// 处理最后一个map，除非Redis info 命令结果集，最后一行不是空行或者nil的标识
+
+	info[info_title] = subInfo
+
+	//重置子 map
+
+	subInfo = nil
+
+	info_title = ""
+
+	return info, nil
 }
