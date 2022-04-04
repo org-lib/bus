fe6b3f768ddd8ea282f42b8ef6775f01872ff23e
org-lib <96509210+org-lib@users.noreply.github.com>
2022-04-04 19:49:02 +0800

    Update msijson.go

--- msi/msijson.go
+++ msi/msijson.go
@@ -60,6 +60,57 @@ var (
     "license-url": "xxxxxx"
   }
 }`
+	NoFileJosn = `{
+  "product": "xxxxxx",
+  "company": "shangguannihao",
+  "license": "LICENSE",
+  "upgrade-code": "",
+  "files": {
+    "guid": "",
+    "items": [
+      "build/amd64/xxxxxx.exe"
+    ]
+  },
+  "directories": [
+    "assets"
+  ],
+  "env": {
+    "guid": "",
+    "vars": [
+      {
+        "name": "some",
+        "value": "value",
+        "permanent": "no",
+        "system": "no",
+        "action": "set",
+        "part": "last"
+      }
+    ]
+  },
+  "shortcuts": {
+    "guid": "",
+    "items": [
+      {
+        "name": "xxxxxx",
+        "description": "xxxxxx web server",
+        "target": "[INSTALLDIR]\\xxxxxx.exe",
+        "wdir": "INSTALLDIR",
+        "icon":"assets/ico.ico"
+      }
+    ]
+  },
+  "hooks": [
+    {"when": "install", "command": "sc.exe create XxxxxxSvc binPath=\"[INSTALLDIR]xxxxxx.exe\" type=share start=auto DisplayName=\"xxxxxx\""},
+    {"when": "install", "command": "sc.exe start XxxxxxSvc"},
+    {"when": "uninstall", "command": "sc.exe delete XxxxxxSvc"}
+  ],
+  "choco": {
+    "description": "xxxxxx program",
+    "project-url": "xxxxxx",
+    "tags": "xxxxxx",
+    "license-url": "xxxxxx"
+  }
+}`
 )
 
 //定义MSI信息
@@ -76,7 +127,12 @@ type Msi struct {
 func SetJson(svc string, name string, filetype string, cnf string, filename string) {
 	v := viper.New()
 	v.SetConfigType(filetype) // 设置配置文件的类型
-	fj := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(FileJosn, "xxxxxx", name), "Xxxxxx", svc), "yyyyyy.cnf", cnf)
+	fj := ""
+	if strings.Trim(cnf, " ") != "" {
+		fj = strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(FileJosn, "xxxxxx", name), "Xxxxxx", svc), "yyyyyy.cnf", cnf)
+	} else {
+		fj = strings.ReplaceAll(strings.ReplaceAll(NoFileJosn, "xxxxxx", name), "Xxxxxx", svc)
+	}
 	v.ReadConfig(bytes.NewBuffer([]byte(fj)))
 	v.WriteConfigAs(filename)
 }
