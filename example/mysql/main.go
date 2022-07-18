991537fda78bd9944fec8bf69168083980f24a3c
org-lib <96509210+org-lib@users.noreply.github.com>
2022-07-18 15:23:35 +0800

    Update main.go

--- example/mysql/main.go
+++ example/mysql/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/org-lib/bus/db/mysql"
 	"github.com/org-lib/bus/logger"
 	"go.uber.org/zap"
+	"strings"
 )
 
 func main() {
@@ -19,7 +20,7 @@ func main() {
 	cfg = &mysql.Info{
 		Host:         config.Config.V.GetString("mysql.host"),
 		Port:         config.Config.V.GetString("mysql.port"),
-		Database:     "mysql",
+		Database:     config.Config.V.GetString("mysql.database"),
 		Username:     config.Config.V.GetString("mysql.username"),
 		Password:     config.Config.V.GetString("mysql.password"),
 		Timeout:      3000,
@@ -27,7 +28,6 @@ func main() {
 		WriteTimeout: 3000,
 		Charset:      "utf8",
 	}
-	cfg.Database = "mysql"
 
 	//获取数据库实例连接
 	db, err := mysql.Open(cfg)
@@ -39,12 +39,12 @@ func main() {
 
 	//unix/linux 可用 sqlutils
 	//sqlutils.QueryRowsMap 不支持在Windows 上运行，log 包异常
-	err = sqlutils.QueryRowsMap(db, `select * from help_topic where name = ?`, func(m sqlutils.RowMap) error {
-		logger.Log.Info("数据库返回信息：", zap.String("name", m.GetString("name")))
-		logger.Log.Info("数据库返回信息：", zap.String("help_topic_id", m.GetString("help_topic_id")))
+	err = sqlutils.QueryRowsMap(db, `select * from sys_config where id = ?`, func(m sqlutils.RowMap) error {
+		logger.Log.Info("数据库返回信息：", zap.String("config_value", m.GetString("config_value")))
 		logger.Log.Info("数据库返回信息：", zap.String("description", m.GetString("description")))
+		logger.Log.Info("数据库返回信息：", zap.String("saas_tenant_code", m.GetString("saas_tenant_code")))
 		return nil
-	}, "HEX")
+	}, "1")
 	if err != nil {
 		logger.Log.Error(fmt.Sprintf("数据库查询，失败：%v", err), zap.String("mysql", "运行失败！"))
 	}
@@ -60,7 +60,36 @@ func main() {
 	}
 
 	//日志打印
-	logger.Log.Info("server start", zap.String("mysql", "运行结束！"))
+	tmpResults, err := sqlutils.QueryNamedResultData(db, fmt.Sprintf("select id from trade_task limit 10"))
+	// insert all
+	// 根据 主键 批次查询 并发插入
+
+	// insert 头部
+	tmp := "insert into tempTable("
+	for _, n := range tmpResults.Columns {
+		tmp = tmp + n + ","
+	}
+	tmp = strings.TrimRight(tmp, ",")
+	tmp = tmp + ")values"
+	//insert 数据部分
+	tmpv := ""
+	logger.Log.Info(fmt.Sprintf("v-1=%v", len(tmpResults.Data)))
+	for _, datum := range tmpResults.Data {
+		tmpx := ""
+		for _, data := range datum {
+			if !data.Valid {
+				tmpx = tmpx + "," + "NULL"
+
+			} else {
+
+				tmpx = tmpx + "," + "'" + data.String + "'"
+			}
+		}
+		logger.Log.Info(fmt.Sprintf("v0=(%v)", strings.TrimLeft(tmpx, ",")))
+		tmpv = tmpv + "," + "(" + strings.TrimLeft(tmpx, ",") + ")"
+	}
+	logger.Log.Info(fmt.Sprintf("v1=%v%v", tmp, strings.TrimLeft(tmpv, ",")))
+
 	//其它操作方式不断迭代中...
 
 }
