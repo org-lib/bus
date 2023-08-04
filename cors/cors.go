568fc6e90a427375405cceebe0c0b5eef0658ca0
org-lib <shangguannihao@gmail.com>
2023-08-04 11:27:42 +0800

    Update cors.go

--- cors/cors.go
+++ cors/cors.go
@@ -39,3 +39,23 @@ func ECors() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+// 处理跨域请求,支持options访问
+func CorsPlus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Expose-Headers", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		//放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		// 处理请求
+		c.Next()
+	}
+}
